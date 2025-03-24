f44d569d3029c33b1556da1285ae98e12da72ae7
Berkan Çalışkan <def.bcaliskan@softtech.com.tr>
2025-03-24 19:08:40 +0300

    some changes

--- Package.swift
+++ Package.swift
@@ -6,20 +6,17 @@ import PackageDescription
 let package = Package(
     name: "IsbankBankingServices",
     platforms: [
-        .iOS(.v14),
-        .visionOS(.v1)
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: "IsbankBankingServices",
-            targets: ["IsbankBankingServices"]
-        ),
+            targets: ["IsbankBankingServices"])
     ],
     targets: [
         .binaryTarget(
             name: "IsbankBankingServices",
             url: "https://nexusout.isbank.com.tr/repository/ios-raw/getir-sdkdeneme/1.0.0/IsbankBankingServices.xcframework.zip",
-            checksum: "fd777145c51d6abc93e766c28edc58875dbd7dadd928ff1c07b91e3599b5602c"
-        )
+            checksum: "fd777145c51d6abc93e766c28edc58875dbd7dadd928ff1c07b91e3599b5602c")
     ]
 )
